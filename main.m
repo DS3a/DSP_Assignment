72e8f7afc089ab60651150c864c37a689cbe9fe1
DS3a <deepesh.padala02@gmail.com>
2022-04-26 23:25:38 +0530

    gotta improve

--- main.m
+++ main.m
@@ -28,26 +28,63 @@ no4 = remove_noise(no4);
 no5 = remove_noise(no5);
 
 
-no = xcorr(no1, no2);
-yes = xcorr(no1, yes1);
+no = xcorr(no4, no1);
+yes = xcorr(no4, yes1);
 
 subplot(2, 1, 1);
 stem(no);
-ylim([-100 100])
+ylim([0 1000])
 title('correlation of no4 with no1 signal');
 
 
 subplot(2, 1, 2)
 stem(yes);
-ylim([-100 100])
+ylim([0 1000])
 title('correlation of no4 with yes1 signal')
 
 
-disp("Average amplitude of correlation of no with no : ")
+disp("Average amplitude of correlation of no4 with no1 : ")
 disp(abs(mean(no)));
 
-disp("Average amplitude of correlation of no with yes : ")
+disp("Average amplitude of correlation of no4 with yes1 : ")
 disp(abs(mean(yes)));
 
-disp("As you can see, the averrage amplitude of similar signals when correlated is higher");
-disp("We can use this to check whether the user has responded yes or no");
+disp("\n\nAverage amplitude of correlation of no4 with no2 : ")
+disp(abs(mean(xcorr(no4, no2))));
+
+disp("Average amplitude of correlation of no4 with yes2 : ")
+disp(abs(mean(xcorr(no4, yes2))));
+
+
+disp("\n\nAverage amplitude of correlation of no4 with no3 : ")
+disp(abs(mean(xcorr(no4, no3))));
+
+disp("Average amplitude of correlation of no4 with yes3 : ")
+disp(abs(mean(xcorr(no4, yes3))));
+
+
+
+
+
+
+disp("\n\n\n\nAverage amplitude of correlation of yes4 with no1 : ")
+disp(abs(mean(xcorr(yes2, no1))));
+
+disp("Average amplitude of correlation of yes4 with yes1 : ")
+disp(abs(mean(xcorr(yes2, yes1))));
+
+disp("\n\nAverage amplitude of correlation of yes4 with no2 : ")
+disp(abs(mean(xcorr(yes2, no2))));
+
+disp("Average amplitude of correlation of yes4 with yes2 : ")
+disp(abs(mean(xcorr(yes2, yes2))));
+
+
+disp("\n\nAverage amplitude of correlation of yes4 with no3 : ")
+disp(abs(mean(xcorr(yes2, no3))));
+
+disp("Average amplitude of correlation of yes4 with yes3 : ")
+disp(abs(mean(xcorr(yes2, yes3))));
+
+%disp("As you can see, the averrage amplitude of similar signals when correlated is higher");
+%disp("We can use this to check whether the user has responded yes or no");
--- remove_noise.m
+++ remove_noise.m
@@ -1,5 +1,5 @@
  function clear_audio = remove_noise(noisy_audio)
   
-  clear_audio = noisy_audio;
+  clear_audio = abs(noisy_audio);
 %  disp(noisy_audio);
 end
